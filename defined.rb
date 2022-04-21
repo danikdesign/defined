1a024f83cfcfa594ce80e2abb271dd24ab49f4c4
danikdesign <danik.newmail@gmail.com>
2022-04-21 15:56:34 +0300

    v 2.0

--- defined.rb
+++ defined.rb
@@ -1,3 +1,5 @@
+######################################
+=begin
 b = 5
 
 if defined?(a)
@@ -11,3 +13,27 @@ if defined?(b)
 else
   puts "Переменная 'b' неопределена"
 end
+=end
+#######################################
+# that was old version of this program
+#
+# this is v 2.0
+
+def method
+  c = 3
+  puts "Переменная '$a': #{defined?($a)}"
+  puts "Переменная 'b': #{defined?(b)}"
+  puts "Переменная 'c': #{defined?(c)}"
+end
+
+$a = 1
+
+b = 2
+
+method
+
+puts "Переменная '$a': #{defined?($a)}"
+puts "Переменная 'b': #{defined?(b)}"
+puts "Переменная 'c': #{defined?(c)}"
+
+
