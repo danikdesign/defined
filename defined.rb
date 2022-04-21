ba337694d5281287ad789851aac9269e32fd474b
danikdesign <danik.newmail@gmail.com>
2022-04-21 16:35:17 +0300

    class variable check

--- defined.rb
+++ defined.rb
@@ -17,8 +17,10 @@ end
 #######################################
 # that was old version of this program
 #
+######################################
 # this is v 2.0
-
+#
+=begin
 def method
   c = 3
   puts "Переменная '$a': #{defined?($a)}"
@@ -35,5 +37,36 @@ method
 puts "Переменная '$a': #{defined?($a)}"
 puts "Переменная 'b': #{defined?(b)}"
 puts "Переменная 'c': #{defined?(c)}"
+=end
+######################################
+# that was 2.0 version of this program
+#
+######################################
+# this is class_variable_defined version
+#
+
+variable = 1
+
+class MyClass
+  def initialize(variable)
+    @variable = variable
+  end
+
+  def check_variables
+    puts "В классе:"
+    puts "Переменная 'variable': #{defined?(variable)}"
+    puts "Переменная '@variable': #{defined?(@variable)}"
+  end
+end
+
+obj = MyClass.new(variable)
+
+obj.check_variables
+
+puts "Вне класса:"
+puts "Переменная 'variable': #{defined?(variable)}"
+puts "Переменная '@variable': #{defined?(@variable)}"
+
+
 
 
